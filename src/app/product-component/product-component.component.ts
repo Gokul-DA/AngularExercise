60b9182d7b10825a6e4e1846301f35c5328cd721
Gokul-Da <gokul.tn.ind@gmail.com>
2023-10-03 09:01:00 +0530

    Exercise 4 : chose a category added.

--- src/app/product-component/product-component.component.ts
+++ src/app/product-component/product-component.component.ts
@@ -3,11 +3,13 @@ import { Product } from './product';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product-component.component.1.html',
+  templateUrl: './product-component.component.2.html',
   styleUrls: ['./product-component.component.css'],
 })
 export class ProductComponent {
   products: Product[];
+  categories: string[] = ['Category 1', 'Category 2', 'Category 3'];
+  selectedCategory: string = 'Category 1';
   singleProduct: Product = new Product(
     1,
     'Description 123',
@@ -20,9 +22,9 @@ export class ProductComponent {
     this.products = [
       new Product(1, 'Description 1', 10, 100, 'Category 1', 'Manufacturer 1'),
       new Product(2, 'Description 2', 10, 100, 'Category 2', 'Manufacturer 2'),
-      new Product(3, 'Description 3', 10, 100, 'Category 3', 'Manufacturer 3'),
-      new Product(4, 'Description 4', 10, 100, 'Category 4', 'Manufacturer 4'),
-      new Product(5, 'Description 5', 10, 100, 'Category 5', 'Manufacturer 5'),
+      new Product(3, 'Description 3', 10, 100, 'Category 1', 'Manufacturer 3'),
+      new Product(4, 'Description 4', 10, 100, 'Category 2', 'Manufacturer 4'),
+      new Product(5, 'Description 5', 10, 100, 'Category 3', 'Manufacturer 5'),
     ];
   }
   createProducts(product1: Product) {
